31f9b11cf2bc5200cdc9288e5719f02dac6e2cc4
Shane Eire Byrne <shane.e.byrne@gmail.com>
2020-11-05 20:27:51 -0800

    Add files via upload

--- studentAidDatabase.sql
+++ studentAidDatabase.sql
@@ -140,6 +140,31 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `studentAidDatabase`.`distByAge`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `studentAidDatabase`.`distByAge` (
+  `grantsDisb` INT NOT NULL,
+  `grantsRec` INT NULL,
+  `loansDist` INT NULL,
+  `loansRec` INT NULL,
+  `totalDist` INT NULL,
+  `totalRec` INT NULL,
+  `age` INT NULL,
+  `grants_has_distByAge_grants_grantDisb` DOUBLE NOT NULL,
+  `grants_has_distByAge_grants_grantDist` DOUBLE NOT NULL,
+  `grants_has_distByAge_distByAge_grantsDisb` DOUBLE NOT NULL,
+  PRIMARY KEY (`grantsDisb`),
+  INDEX `fk_distByAge_grants_has_distByAge1_idx` (`grants_has_distByAge_grants_grantDisb` ASC, `grants_has_distByAge_grants_grantDist` ASC, `grants_has_distByAge_distByAge_grantsDisb` ASC) ,
+  CONSTRAINT `fk_distByAge_grants_has_distByAge1`
+    FOREIGN KEY (`grants_has_distByAge_grants_grantDisb` , `grants_has_distByAge_grants_grantDist` , `grants_has_distByAge_distByAge_grantsDisb`)
+    REFERENCES `studentAidDatabase`.`grants_has_distByAge` (`grants_grantDisb` , `grants_grantDist` , `distByAge_grantsDisb`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+COMMENT = '	';
+
+
+-- -----------------------------------------------------
 -- Table `studentAidDatabase`.`grants`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `studentAidDatabase`.`grants` (
@@ -162,3 +187,59 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE "grants-ay18-19-q3.xls" INTO TABLE `studentAidDatabase`.`grantVol`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE "GE2011InformationalRates102512_0.XLS" INTO TABLE `studentAidDatabase`.`gainfulEmployment`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; 
+
+LOAD DATA LOCAL INFILE "FL_Dashboard_AY2009_2010_Q4.xls" INTO TABLE `studentAidDatabase`.`ffelVol`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; 
+
+LOAD DATA LOCAL INFILE "dl-dashboard-ay2018-2019-q3.xls" INTO TABLE `studentAidDatabase`.`dlVolschool`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; 
+
+LOAD DATA LOCAL INFILE "AY18Disbursements-by-Program.xls" INTO TABLE `studentAidDatabase`.`distributedByProgram`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; 
+
+LOAD DATA LOCAL INFILE "AY18Disbursements-by-Location.xls" INTO TABLE `studentAidDatabase`.`disbursedByLocation`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; 
+
+LOAD DATA LOCAL INFILE "AY18Disbursements-by-Age.xls" INTO TABLE `studentAidDatabase`.`distByAge`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; 
+
+LOAD DATA LOCAL INFILE "1920FedSchoolCodeList.xlsx" INTO TABLE `studentAidDatabase`.`schoolCode`
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS; 
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+/*Week 3 inquiries*/
+SELECT * FROM   `studentAidDatabase`.`schoolCode_has_greatfulEmployment`  AS t1
+CROSS JOIN `studentAidDatabase`.`greatfulEmployment_has_ffelVol` AS t2
+ WHERE fk_greatfulEmployment_has_grantVol_greatfulEmployment1_idx > 1000;
+ 
+SELECT * FROM `studentAidDatabase`.`grantVol_has_schoolCode` AS t3;
+SELECT `studentAidDatabase`.`distByAge` AS t4;
+SELECT AVG(t4.`loansDist`);
+ 
+ 
+ 
